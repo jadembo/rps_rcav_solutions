92e0ba10e4c7fcd2d1eb619a02ec8f14eb217dc3
Justin A. Dembo <jdembo@chicagobooth.edu>
2016-10-25 18:53:35 -0500

    Simplified into One View, One Route, One Controller

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,43 +1,33 @@
 class GameController < ApplicationController
-  def user_plays_rock
+  def user_plays_anything
     @computer_move = ["rock", "paper", "scissors"].sample
+    @user_move = params["move"]
 
-    if @computer_move == "rock"
-      @outcome = "tied"
-    elsif @computer_move == "paper"
-      @outcome = "lost"
-    elsif @computer_move == "scissors"
-      @outcome = "won"
+    if @user_move == "rock"
+      if @computer_move == "rock"
+        @outcome = "tied"
+      elsif @computer_move == "paper"
+        @outcome = "lost"
+      elsif @computer_move == "scissors"
+        @outcome = "won"
+      end
+    elsif @user_move == "scissors"
+      if @computer_move == "rock"
+        @outcome = "lost"
+      elsif @computer_move == "paper"
+        @outcome = "won"
+      elsif @computer_move == "scissors"
+        @outcome = "tied"
+      end
+    elsif @user_move=="paper"
+      if @computer_move == "rock"
+        @outcome = "won"
+      elsif @computer_move == "paper"
+        @outcome = "tied"
+      elsif @computer_move == "scissors"
+        @outcome = "lost"
+      end
     end
-
-    render("user_plays_rock.html.erb")
-  end
-
-  def user_plays_paper
-    @computer_move = ["rock", "paper", "scissors"].sample
-
-    if @computer_move == "rock"
-      @outcome = "won"
-    elsif @computer_move == "paper"
-      @outcome = "tied"
-    elsif @computer_move == "scissors"
-      @outcome = "lost"
-    end
-
-    render("user_plays_paper.html.erb")
-  end
-
-  def user_plays_scissors
-    @computer_move = ["rock", "paper", "scissors"].sample
-
-    if @computer_move == "rock"
-      @outcome = "lost"
-    elsif @computer_move == "paper"
-      @outcome = "won"
-    elsif @computer_move == "scissors"
-      @outcome = "tied"
-    end
-
-    render("user_plays_scissors.html.erb")
+    render("user_plays_anything.html.erb")
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
-  get("/", { :controller => "game", :action => "user_plays_rock" })
+  get("/", { :controller => "game", :action => "user_plays_anything" })
 
-  get("/rock", { :controller => "game", :action => "user_plays_rock" })
-
-  get("/paper", { :controller => "game", :action => "user_plays_paper" })
-
-  get("/scissors", { :controller => "game", :action => "user_plays_scissors" })
+  get("/:move", {:controller => "game", :action => "user_plays_anything"})
 end
